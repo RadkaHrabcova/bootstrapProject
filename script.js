ad2eec2ed2adf3626ed87e386e79df904d207ed0
RadkaHrabcova <radkahrabcova@gmail.com>
2024-09-08 22:43:34 +0200

    Italian restaurant practising bootstrap

---
+++ script.js
@@ -0,0 +1,9 @@
+function openMenu() {
+    var menu = document.getElementById("menu");
+    menu.style.display = "block";
+};
+
+/*function openMenuNav(){
+    var navMenu = document.getElementById("nav-menu");
+    navMenu.style.display = "block";
+};*/
